f0a5e39f594e441c62325eda177129cdcbd98e47
QuinnPooch <pucciquinn@gmail.com>
2022-01-07 12:43:16 -0700

    added directory for routes, created and completed html routes

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const apiRoutes = require('./api-routes/apiroutes');
+const htmlRoutes = require('./api-routes/htmlroutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('');
-const htmlRoutes = require('');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
